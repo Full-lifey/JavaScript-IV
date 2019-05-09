c26d6d0656fb34adf72fa76ed05e838668d75644
Full-lifey <44554249+Full-lifey@users.noreply.github.com>
2019-05-09 14:52:14 -0600

    STRETCH: Completed

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -23,6 +23,17 @@ class Instructor extends Person{
     grade(student, subject) {
         return `${student.name} receives a perfect score on ${subject}`
     }
+    gradeAssignments(student) {
+        let i = 0
+        while (i<5) {
+            let randomGrade = Math.round((Math.random() * 10)-5);
+            console.log(randomGrade)
+            student.grade += randomGrade
+            console.log(student.grade)
+            i++
+        }
+        return student.graduate()
+    }
 }
 
 class Student extends Person{
@@ -31,6 +42,7 @@ class Student extends Person{
         this.previousBackground = studentAttributes.previousBackground;
         this.className = studentAttributes.className;
         this.favSubjects = studentAttributes.favSubjects;
+        this.grade = studentAttributes.grade;
     }
     listsSubjects() {
         this.favSubjects.forEach(subject => console.log(subject))
@@ -40,6 +52,13 @@ class Student extends Person{
     }
     sprintChallenge(subject) {
         return `${this.name} has begun sprint challenge on ${subject}`
+    }
+    graduate() {
+        if (this.grade >= 70) {
+            return `Congratulations! ${this.name} has graduated!`
+        } else {
+            return `${this.name} has a grade of ${this.grade}%. Keep working ${this.name} you will get there some day!`
+        }
     }
 }
 
@@ -84,7 +103,8 @@ const joel = new Student({
     catchPhrase: `You just don't see that every day`,
     previousBackground: 'Sys Admin',
     className: 'Web20',
-    favSubjects: ['Javascript', 'Python', 'Bitcoin']
+    favSubjects: ['Javascript', 'Python', 'Bitcoin'],
+    grade: 95
 }); 
 
 const jared = new Student({
@@ -96,7 +116,8 @@ const jared = new Student({
     catchPhrase: `I'm screwed for the sprint challenge`,
     previousBackground: 'Land surveyer',
     className: 'Web20',
-    favSubjects: ['HTML', 'CSS', 'Golf']
+    favSubjects: ['HTML', 'CSS', 'Golf'],
+    grade: 70
 }); 
 
 const ryan = new ProjectManager({
@@ -129,3 +150,4 @@ console.log(jared.sprintChallenge('Intro to Javascript'));
 console.log(ryan.standUp('#web20_ryan'))
 console.log(ryan.debugsCode(jared, 'Javascript III'))
 console.log(ian.catchPhrase)
+console.log(dan.gradeAssignments(jared))
